7b2555316c4c0f41366be8ea1e3100ab1ce05766
larbikhounti <larbikhounti@gmail.com>
2020-09-24 13:33:55 +0100

    Updates

---
+++ precache-manifest.19b87b05a435c30523111e4c39058ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8526270b222f7e9fe9b88c0a7aa369",
+    "url": "/adslab/index.html"
+  },
+  {
+    "revision": "e1d8c3040fb1b33d24d8",
+    "url": "/adslab/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "23179becdf0a951432b4",
+    "url": "/adslab/static/js/2.a9b78d44.chunk.js"
+  },
+  {
+    "revision": "d8d89644747ddd534f45f20a40dd34ae",
+    "url": "/adslab/static/js/2.a9b78d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d8c3040fb1b33d24d8",
+    "url": "/adslab/static/js/main.922bd035.chunk.js"
+  },
+  {
+    "revision": "6f9dc3c3f631be236ae3",
+    "url": "/adslab/static/js/runtime-main.5a551993.js"
+  }
+]);
